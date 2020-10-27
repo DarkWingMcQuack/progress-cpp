7a02a1847d6432c994d7d283ea3dc1a2d0ddff8e
lukas <np-hardass@protonmail.com>
2020-10-27 17:28:45 +0100

    delete outdated comment

--- example/src/example.cpp
+++ example/src/example.cpp
@@ -7,11 +7,6 @@ auto main() -> int
 {
     const int total = 10000;
 
-    /*
-     * Define a progress bar that has a total of 10000,
-     * a width of 70, shows `#` to indicate completion
-     * and a dash '-' for incomplete
-     */
     progresscpp::ProgressBar progress_bar0(total, 70);
 
     for(int i = 0; i < total; i++) {
